651cce11f4c4a89386fa623584bd6cac2313b763
lucasrodlima <lucasrodlima27@gmail.com>
2025-04-06 17:06:32 -0300

    feat: add constants module and start screen

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
 def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill("black")
+
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
