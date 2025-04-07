a545862167a5622d2ff6ee6fe877e4fef9b79de1
lucasrodlima <lucasrodlima27@gmail.com>
2025-04-07 05:07:02 -0300

    refactor(main): remove boilerplate

--- main.py
+++ main.py
@@ -8,17 +8,13 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -30,9 +26,6 @@ def main():
         player.draw(screen)
 
         pygame.display.flip()
-
-        dt = clock.tick(60) / 1000
-        print(dt)
 
 
 if __name__ == "__main__":
